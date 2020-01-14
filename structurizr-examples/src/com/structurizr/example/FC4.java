af773fbc57a97df9c992d5f2e9c396684ca0855b
Simon Brown <simon@structurizr.com>
2020-01-14 10:56:31 +0000

    Add an example of how to export to C4-PlantUML.

--- structurizr-examples/src/com/structurizr/example/FC4.java
+++ structurizr-examples/src/com/structurizr/example/FC4.java
@@ -3,13 +3,10 @@ package com.structurizr.example;
 import com.structurizr.Workspace;
 import com.structurizr.api.StructurizrClient;
 import com.structurizr.io.fc4.Fc4Importer;
-import com.structurizr.util.WorkspaceUtils;
-import org.yaml.snakeyaml.Yaml;
+import com.structurizr.io.plantuml.C4PlantUMLWriter;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.Map;
+import java.io.StringWriter;
 
 /**
  * Imports software architecture models defined in the fc4 YAML DSL.
@@ -24,11 +21,18 @@ public class FC4 {
 
     public static void main(String[] args) throws Exception {
         File fc4Directory = new File("./structurizr-examples/src/com/structurizr/example/fc4/bigbankplc");
-        Workspace workspace = new Fc4Importer("Big Bank plc", "This is an example workspace to illustrate the key features of Structurizr, based around a fictional online banking system.", fc4Directory).importWorkspace();
+        Workspace workspace = new Fc4Importer("Big Bank plc", "This is an example workspace, imported from an FC4 YAML model definition.", fc4Directory).importWorkspace();
 
+        // upload the workspace to Structurizr
         StructurizrClient structurizrClient = new StructurizrClient(API_KEY, API_SECRET);
         structurizrClient.setMergeFromRemote(false);
         structurizrClient.putWorkspace(WORKSPACE_ID, workspace);
+
+        // example of how to export a Structurizr workspace to C4-PlantUML
+        C4PlantUMLWriter c4PlantUMLWriter = new C4PlantUMLWriter();
+        StringWriter stringWriter = new StringWriter();
+        c4PlantUMLWriter.write(workspace, stringWriter);
+        System.out.println(stringWriter.toString());
     }
 
 }
